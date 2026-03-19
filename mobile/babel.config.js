08e84e6feaf050c767c89665c67dd64905c68352
harinnig <harinnig1@gmail.com>
2026-03-19 15:23:04 +0530

    New updation

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,13 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
-
   return {
-    presets: ['babel-preset-expo'], // ✅ Required for Expo
-
-    plugins: [
-      // (optional plugins can go here)
-
-      'react-native-reanimated/plugin', // ✅ MUST ALWAYS BE LAST
-    ],
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
